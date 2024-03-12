e15f551b412d2b427432bd182b1b9626d174fab1
edisxon <edisxon10@gmail.com>
2024-03-11 22:15:14 -0500

    no estoy seguro de como ver si el alert funcionaria ajjaja

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,10 +2,14 @@ console.log("vinculado el js")
 
 
 
+const axios = require('axios')
 const transformer = require("./tranformer")
 
-$.get("https://students-api.up.railway.app/movies", (data) => {
-    
-    
+axios.get("https://students-api.up.railway.app/movies")
+  .then(response => {
+    const data = response.data
     data.forEach(movie => transformer(movie))
-})
+  })
+  .catch(error => {
+    alert("Error al obtener los datos: " + error.message)
+  })
